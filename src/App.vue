9d5d8cbc188ae5a4b34a3043408f8aec23eccedc
Quentin <quentin.goujon@epita.fr>
2022-08-23 12:32:45 -0400

    oui

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,39 @@
-
 <script setup>
-import { ref, onMounted } from 'vue'
+import {ref, onMounted} from 'vue'
 
 
-
-const money = ref(5)
+const money = ref(10)
 const roulette = ref(0)
-const gains = [];
+const gains = ref([])
+const typeOfMiseSelected = ref(1)
 
-const colors = ref('red');
-
-const mises = {
-  red : ref(0),
-  black : ref(0),
-  even : ref(0),
-  odd : ref(0),
-  first_18 : ref(0),
-  end_19 : ref(0),
-}
-
+const mises = ref([
+  {id: 1, title: '1st 12', value: 0, gain: 3, span: '1 / 3', back: 'green', win:false,  condition: (val) => val > 0 && val < 13} ,
+  {id: 2, title: '2nd 12', value: 0, gain: 3, span: '3/5',back: 'green', win:false, condition: (val) => val > 12 && val < 25},
+  {id: 3, title: '3rd 12', value: 0, gain: 3, span: '5/7',back: 'green',win:false,  condition: (val) => val > 24},
+  {id: 4, title: '1 To 18', value: 0, gain: 2, span: '1',back: 'green',win:false,  condition: (val) => val > 0 && val < 18},
+  {id: 5, title: 'EVEN', value: 0, gain: 2, span: '2',back: 'green', win:false, condition: (val) => val % 2 == 0},
+  {id: 6, title: 'R', value: 0, gain: 2, span: '3',back: 'red', win:false, condition: (val) => red_number.includes(val)},
+  {id: 7, title: 'B', value: 0, gain: 2, span: '4',back: 'black', win:false, condition: (val) => !red_number.includes(val)},
+  {id: 8, title: 'ODD', value: 0, gain: 2, span: '5',back: 'green',win:false,  condition: (val) => val % 2 == 1},
+  {id: 9, title: '19 To 36', value: 0, gain: 2, span: '6',back: 'green', win:false, condition: (val) => val >= 19},
+])
 const red_number = [
   1, 3, 5, 7, 9, 12, 14, 16, 18, 19, 21, 23, 25, 27, 30, 32, 34, 36
 ]
+const typeOfMise = [1,5,25,100]
 
 
-
-function Increment(ref_mise)
-{
-  console.log(mises)
-  if(money.value > 0)
-  {
-    ref_mise.value++
-    money.value--
+function Increment(ref_mise) {
+  if (money.value >= typeOfMiseSelected.value) {
+    ref_mise.value+=typeOfMiseSelected.value
+    money.value-=typeOfMiseSelected.value
+  }
+}
+function Decrement(ref_mise) {
+  if (ref_mise.value >= typeOfMiseSelected.value) {
+    ref_mise.value-=typeOfMiseSelected.value
+    money.value+=typeOfMiseSelected.value
   }
 }
 
@@ -40,39 +42,16 @@ function PlayRoulette() {
   var nombre = Math.floor(Math.random() * (36));
   roulette.value = nombre
 
-  //calcul des gains
   var gain = 0;
-  if(nombre % 2 == 0){
-    gain +=  mises.even.value * 2;
-  }
-  else{
-    gain += mises.odd.value *2
-  }
-  if(red_number.includes(nombre)){
-    gain += mises.red.value *2
-    colors.value = 'red'
-  }
-  else{
-    gain += mises.black.value *2
-    colors.value = 'black'
-  }
-  if(nombre < 19){
-    gain += mises.first_18.value *2
-  }
-  else{
-    gain += mises.end_19.value *2
-  }
-
-  //reset all mises
-  for(var key in mises)
-  {
-    mises[key].value = 0
-  }
-
-  console.log("Gain de ",gain);
-  money.value += gain;
-  gains.push(gain);
-
+  mises.value.forEach((mise) => {
+    if (mise.condition(nombre)) {
+      gain += mise.value * mise.gain;
+      money.value += mise.value * mise.gain;
+    }
+    gain -= mise.value
+    mise.value = 0
+  })
+  gains.value.unshift(gain)
 }
 
 // lifecycle hooks
@@ -83,43 +62,56 @@ onMounted(() => {
 
 <template>
   <div class="header">
-    <span>{{money}} 💲</span>
+    <span>{{ money }} 💲</span>
   </div>
   <div class="gains">
-    <span>Gains : {{gains}}</span>
+    <ul v-for="gain in gains" :key="gain">
+      <div class="gain">
+        {{ gain > 0 ? "➕" : "➖" }}
+        {{ Math.abs(gain) }}
+      </div>
+    </ul>
   </div>
   <div class="block_body">
     <div class="block_roulette">
-      <div class="roulette" :class="{color : colors}">
-        <span>{{roulette}}</span>
+      <div class="roulette" :style="{ backgroundColor : red_number.includes(roulette) ? 'red' : 'black' }">
+        <span>{{ roulette }}</span>
       </div>
       <button class="play" @click="PlayRoulette">Play</button>
     </div>
     <div class="block_mise">
-      <div class="block_mise_1">
-        <div class="block_mise_1_item" 
-          @click="Increment(mises.first_18)"
-          >1 to 18</div>
-        <div class="block_mise_1_item"
-          @click="Increment(mises.even)"
-        >EVEN</div>
-        <div class="block_mise_1_item"
-          @click="Increment(mises.red)"
-          >🔴</div>
-        <div class="block_mise_1_item"
-          @click="Increment(mises.black)"
-          >⚫</div>
-        <div class="block_mise_1_item"
-          @click="Increment(mises.odd)"
-          >ODD</div>
-        <div class="block_mise_1_item"
-          @click="Increment(mises.end_19)"
-          >19 to 36</div>
-      </div>
+      <ul class="block_mise_1">
+        <div
+            v-for="mise in mises"
+            :key="mise.id"
+            @click="Increment(mise)"
+            v-on:click.right =  "Decrement(mise)"
+            class="block_mise_1_item"
+            :style="{gridColumn: mise.span ? mise.span : '', backgroundColor: mise.back}"
+        >
+          <div>{{ mise.title }}</div>
+          <div
+              class="block_mise_1_item_val"
+              v-if="mise.value > 0"
+          >{{ mise.value }}
+          </div>
+        </div>
+      </ul>
+    </div>
+    <div class="block_somme">
+        <div
+            class="block_somme_item"
+            v-for="item in typeOfMise"
+            :key="item"
+            :style="{backgroundColor: item == typeOfMiseSelected ? '#2df317' : ''}"
+            @click="typeOfMiseSelected = item"
+        >
+         {{item}}
+        </div>
     </div>
   </div>
 
-  
+
 </template>
 
 <style>
@@ -128,66 +120,114 @@ onMounted(() => {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-}
-.header
-{
-    width: 100%;
-    height: 8vh;
-    
-    font-size: 40px;
-    
-}
-.block_body
-{
-  width: 100%;
+  color: white;
+  background-color: green;
+  border: #1df387 6px solid;
+  user-select: none;
 
-  display: flex;
-  
 }
-.block_roulette
-{
+
+.header {
+  margin-top: 20px;
+  width: 100%;
+  height: 8vh;
+  font-size: 40px;
+}
+
+.gains {
+  display: flex;
+  flex-wrap: nowrap;
+  justify-content: left;
+  overflow: hidden;
+  height: 7vh;
+}
+
+.gain {
+  border: black 1px solid;
+  padding: 5px;
+  border-radius: 4px;
+  min-width: 45px;
+}
+
+.block_body {
+  width: 100%;
+  display: flex;
+}
+
+.block_roulette {
   display: flex;
   flex-direction: column;
   padding: 10px;
   margin: 10px;
-  
-  flex : 1;
+  flex: 1;
   height: 50vh;
   align-items: center;
 }
-.block_mise
-{
+
+.block_mise {
   display: flex;
   padding: 10px;
   margin: 10px;
-  
-  flex:3;
+
+  flex: 3;
   height: 50vh;
 }
-.block_mise_1
+.block_somme{
+  flex : 1;
+  display:flex;
+  flex-direction: column;
+  justify-items: center;
+  align-items: end;
+}
+.block_somme_item
 {
+  height: 50px;
+  width: 50px;
+  margin-bottom: 25px;
+  font-size: 24px;
+  border-radius: 25px;
+  border: black 2px dashed;
+  margin-right: 25px;
+  font-weight: bold;
+  padding: 10px;
+}
+.block_mise_1 {
   display: grid;
-  grid-template-columns: 1fr 1fr 1fr 1fr 1fr 1fr;
+  grid-template-columns: repeat(6, 1fr);
   justify-items: stretch;
   align-items: stretch;
   justify-content: stretch;
   align-content: center;
   color: white;
   background-color: green;
-  height: 50px;
+  height: 80px;
   width: 100%;
   font-size: 25px;
   font-weight: bold;
 }
-.block_mise_1_item{
-  height: 100%;
+
+.block_mise_1_item {
+  height: 40px;
+  border: white 1px solid;
 }
-.block_mise_1_item:hover{
+
+.block_mise_1_item:hover , .block_somme_item:hover {
   background-color: #2df317;
 }
-.roulette
-{
+
+.block_mise_1_item_val {
+  background-color: cadetblue;
+  width: 25px;
+  height: 25px;
+  border-radius: 10px;
+  border: black 2px dashed;
+  font-size: 15px;
+  position: relative;
+  left: 40%;
+  bottom: 55%;
+}
+
+.roulette {
   display: inline-block;
   color: white;
   width: 200px;
@@ -198,8 +238,8 @@ onMounted(() => {
   line-height: 3em;
   font-size: 4em;
 }
-.play
-{
+
+.play {
   border: black 1px solid;
   height: 35px;
   width: 80%;
---
+++ src/componants/MiseItem.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>{{title}}-{{value}}</div>
+</template>
+
+<script>
+export default {
+  props:[
+      'title',
+      'value'
+  ],
+  name: "MiseItem",
+  setup(props) {
+    console.log(props.title)
+  }
+
+}
+
+
+
+
+</script>
+
+<style scoped>
+
+</style>
